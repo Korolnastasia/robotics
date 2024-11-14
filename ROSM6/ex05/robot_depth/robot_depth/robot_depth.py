01b564407380ad89543ce613073d79d4723eea93
korol <korolnastasia@gmail.com>
2024-11-14 19:50:03 +0700

    add: mod 6, ex 5

--- ROSM6/ex05/robot_depth/robot_depth/robot_depth.py
+++ ROSM6/ex05/robot_depth/robot_depth/robot_depth.py
@@ -1,43 +1,45 @@
 import rclpy
+from std_msgs.msg import String
 from rclpy.node import Node
-from geometry_msgs.msg import Twist
-from sensor_msgs.msg import LaserScan
 from sensor_msgs.msg import Image
-import time
+from geometry_msgs.msg import Twist
+from cv_bridge import CvBridge, CvBridgeError
+import numpy as np
 
-class Depth_data(Node):
+class Publisher(Node):
+
     def __init__(self):
-        super().__init__('robot_depth')
-        self.publisher_ = self.create_publisher(Twist, '/robot/cmd_vel', 10)
-        self.pose_subscriber = self.create_subscription(Image, '/depth/image', self.update_pose, 10)
-        self.scan = Image()
-        timer_period = 0.5
-        self.timer = self.create_timer(timer_period, self.timer_callback)
+        super().__init__('publisher')
+        self.publisher = self.create_publisher(Twist, '/robot/cmd_vel', 10)
+        self.subscription = self.create_subscription(Image, "/depth/image", self.listener_callback, 10)
+        self.timer = self.create_timer(0.5, self.timer_callback)
+        self.bridge = CvBridge()
+        self.is_obstacle = 0  
         
-    def update_pose(self, data):
-    	self.scan = data
-
     def timer_callback(self):
-        twist_msg = Twist()
-        laser = self.scan.data
-        twist_msg.linear.x = 0.0
-        if (len(laser) != 0):
-        	#self.get_logger().info(f'{laser[179]}')
-        	tmp = int(laser[int(self.scan.width * self.scan.height/2 + self.scan.width/2)])
-        	if (tmp==0.0 or tmp == 127 or tmp == 128):
-                	twist_msg.linear.x = 0.5
-        	else:
-        		twist_msg.linear.x = 0.0
-        twist_msg.linear.z = 0.0
-        self.publisher_.publish(twist_msg)
+        twist = Twist()
+        twist.linear.x = 0.5 if not self.is_obstacle else 0.0
+        self.publisher.publish(twist)
+
+    def listener_callback(self, msg):
+        try:
+            depth_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding='passthrough')
+        except CvBridgeError as e:
+            print(f"CvBridgeError: {e}")
+            return
+
+        depth_array = np.array(depth_image, dtype=np.float32)
+        h, w = depth_array.shape
+        print(depth_array.shape, depth_array)
+
+        self.is_obstacle = 1 if depth_array[120][w // 2] < 2 else 0
 
 def main(args=None):
     rclpy.init(args=args)
-    time.sleep(2)
-    node = Depth_data()
-    rclpy.spin(node)
-    node.destroy_node()
+    circling = Publisher()
+    rclpy.spin(circling)
     rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
+
